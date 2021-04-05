f167d8c7b47529baba2b95e597f964232081c917
Jon Ish <jon_ish@yahoo.com>
2021-04-05 11:31:02 +0100

    Two functions one reads in a CSV into csv reader file, the other reads csv into a pandas dataframe, added header to dataframe

--- analysis.py
+++ analysis.py
@@ -2,4 +2,41 @@
 
 #Author: Jon Ishaque G00398244
 
-print("let's go!")
+
+
+######################################
+#IMPORTS
+import pandas as pd  #alias pd
+from csv import reader
+######################################
+
+
+
+
+def readCSV():
+    with open('iris.data.csv', 'r') as read_obj:
+        csv_reader = reader(read_obj)
+    # Iterate over each row in the csv using reader object
+    
+        for row in csv_reader:
+            # row variable is a list that represents a row in csv
+            print(row[0],row[1],row[2],row[3],row[4])
+
+
+def readintodf():
+    
+    # Read data from file 'filename.csv' 
+    # (in the same directory that your python process is based)
+    # Control delimiters, rows, column names with read_csv (see later) 
+    irisDF = pd.read_csv("iris.data.csv",  header = None, names =["sepal length","sepal width","petal length","petal width","species"] )
+    # Preview the first 5 lines of the loaded data 
+    print(irisDF.head())
+
+
+def main():
+    readintodf()
+
+
+
+if __name__=="__main__":
+    main()

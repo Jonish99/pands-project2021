0f703f164cf2f842db5e2530d9d393b04d39ecf5
Jon Ish <jon_ish@yahoo.com>
2021-04-05 13:48:53 +0100

    print each attribute group summary, from df

--- analysis.py
+++ analysis.py
@@ -14,24 +14,44 @@ from csv import reader
 
 
 def readCSV():
-    with open('iris.data.csv', 'r') as read_obj:
-        csv_reader = reader(read_obj)
-    # Iterate over each row in the csv using reader object
+    #(Lynn, 2021)
+    with open('iris.data.csv', 'r') as reader_object:
+        csv_reader = reader(reader_object)
+
     
         for row in csv_reader:
-            # row variable is a list that represents a row in csv
+            #print at most granular level. 
             print(row[0],row[1],row[2],row[3],row[4])
 
 
 def readintodf():
-    
-    # Read data from file 'filename.csv' 
-    # (in the same directory that your python process is based)
-    # Control delimiters, rows, column names with read_csv (see later) 
+    #(pandas.DataFrame — pandas 1.2.3 documentation, 2021)
+    # Read data from file 'iris.data.csv' 
+ 
+    # read into dataframe
     irisDF = pd.read_csv("iris.data.csv",  header = None, names =["sepal length","sepal width","petal length","petal width","species"] )
     # Preview the first 5 lines of the loaded data 
-    print(irisDF.head())
+    #print(irisDF.head())
 
+    attributes_grouped = irisDF.groupby("species").describe()
+
+    print(attributes_grouped["sepal length"])
+    print(attributes_grouped["sepal width"])
+    print(attributes_grouped["petal length"])
+    print(attributes_grouped["petal width"])
+
+    df1_grouped = irisDF.groupby("species").describe()
+        
+      
+    
+
+    # print species summary
+    #print (irisDF[irisDF["species"] == "Iris-versicolor"].describe())
+
+
+
+def test():
+    x=x
 
 def main():
     readintodf()

f8ac5cea82bb2feb872ded683101a7d3073c1540
Jon Ish <jon_ish@yahoo.com>
2021-04-14 20:05:02 +0100

    added funciton to write variable to single txt file, not four CSVs, using df.to_string()

--- analysis.py
+++ analysis.py
@@ -20,7 +20,7 @@ def cleanLabel(label):
     return label
 def readCSV():
     #(Lynn, 2021)
-    with open('iris.data.csv', 'r') as reader_object:
+    with open('iris_data.csv', 'r') as reader_object:
         csv_reader = reader(reader_object)
         for row in csv_reader:
             #print at most granular level. 
@@ -46,40 +46,73 @@ def createVarSummary(dfTmp,varName):
     
    #Iris_Sum_decimals = pd.Series([2,3,2,3,3,3,2], index=['mean', 'std','min','25%','50%','75%','max']) #(pandas.DataFrame.round — pandas 1.2.3 documentation, 2021)
     dfTmp.round({'mean ':2, 'std ':2,'min ':2,'25% ':2,'50% ':2,'75% ':2,'max ':2}) #Not working
-    ''' 
-    for col in dfTmp.columns:
-        print(col,"!")'''
-    print(dfTmp)
-    #write to file
-    dfTmp.to_csv(varName + ".csv", sep=',', encoding='utf-8')
+   
+    #dfTmp.to_csv("Attributes Grouped" + ".csv", sep=',',  mode='a', encoding='utf-8')
+
+    
+    with open('Iris_Variable_Summaries.txt', 'a') as f:
+        
+       
+        f.write("This is content of iris dataset grouped by variable and summarised."+'\n'+'\n')
+        dfTmp.to_string(f)
+        f.write ('\n'+'\n')
+        f.close()
+
+        
 
 
-def readintodf():
-    #(pandas.DataFrame — pandas 1.2.3 documentation, 2021)
-    # Read data from file 'iris.data.csv' 
-    # read into dataframe
-    iris_df = pd.read_csv("iris.data.csv",  header = None, names =col_names)
-    # Preview the first 5 lines of the loaded data 
-    #print(iris_df.head())
-
-    return(iris_df)
-
-
-def group_by_species(iris_df):
+def group_by_attribute(iris_df):
     attributes_grouped = iris_df.groupby("species").describe()
     #print(attributes_grouped)
     #use attribute names to loop through groups. 
-    for col_name in col_names:
-        
-        if col_name !="species": #do not create summary output for species - it is not a group. Enhancements required
-            print(col_name)
-            #print (attributes_grouped[col_name])
-            #new df based on group
-            dfTmp = attributes_grouped[col_name]
-            createVarSummary(dfTmp,col_name)
+    with open('Iris_Variable_Summaries.txt', 'a') as f:
+        #write file header
+        f.write("This is content of iris dataset grouped by variable and summarised."+'\n'+'\n')
+        for col_name in col_names:
+
+            
+            if col_name !="species": #do not create summary output for species - it is not a group. Enhancements required
+                #write variable header, using col name and cleaning using clean label function
+                f.write (cleanLabel(col_name)+'\n'+'\n')
+                #print (attributes_grouped[col_name])
+                #new df based on group
+                dfTmp = attributes_grouped[col_name]
+                #Iris_Sum_decimals = pd.Series([2,3,2,3,3,3,2], index=['mean', 'std','min','25%','50%','75%','max']) #(pandas.DataFrame.round — pandas 1.2.3 documentation, 2021)
+                dfTmp.round({'mean ':2, 'std ':2,'min ':2,'25% ':2,'50% ':2,'75% ':2,'max ':2}) #Not working
+            
+                
+                dfTmp.to_string(f)
+                f.write ('\n'+'\n')
+    f.close()
 
 
-    
+
+def group_by_species(iris_df):
+    #group by species
+    species_grouped = iris_df.groupby("species")
+    for key, item in species_grouped:
+        print (key)
+        #print(species_grouped.get_group(key), "\n\n")
+ 
+
+
+def  createScatterPlots(iris_df):
+    plt.figure()
+    fig,ax=plt.subplots(1,2,figsize=(21, 10))
+
+    setosa.plot(x="SepalLengthCm", y="SepalWidthCm", kind="scatter",ax=ax[0],label='setosa',color='r')
+    versicolor.plot(x="SepalLengthCm",y="SepalWidthCm",kind="scatter",ax=ax[0],label='versicolor',color='b')
+    virginica.plot(x="SepalLengthCm", y="SepalWidthCm", kind="scatter", ax=ax[0], label='virginica', color='g')
+
+    setosa.plot(x="PetalLengthCm", y="PetalWidthCm", kind="scatter",ax=ax[1],label='setosa',color='r')
+    versicolor.plot(x="PetalLengthCm",y="PetalWidthCm",kind="scatter",ax=ax[1],label='versicolor',color='b')
+    virginica.plot(x="PetalLengthCm", y="PetalWidthCm", kind="scatter", ax=ax[1], label='virginica', color='g')
+
+    ax[0].set(title='Sepal comparasion ', ylabel='sepal-width')
+    ax[1].set(title='Petal Comparasion',  ylabel='petal-width')
+    ax[0].legend()
+    ax[1].legend()
+
 
 
 def test():
@@ -88,15 +121,18 @@ def test():
 def main():
     #create a menu function to make call variousl anayis methods
     #read csv into a df
-    iris_df = readintodf()
-
+    iris_df = pd.read_csv("iris_data.csv",  header = None, names =col_names)
+    #readCSV()
     #print(iris_df)
 
-    #group df
-    createVarHist(iris_df)
-
+    #Create Histo grams based on four variables 
+    #createVarHist(iris_df)
     
-    #create histogram for each variable
+    #write the groupe attributed to a single txt file
+    group_by_attribute(iris_df)
+    
+    #group_by_species(iris_df)
+    
    
 
 
